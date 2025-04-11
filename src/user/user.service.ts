82b101d3c542d204977fc6d7c409e9cafa327374
v_octxiao <v_octxiao@tencent.com>
2025-04-11 14:36:28 +0800

    config path alias, initData

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,11 +5,19 @@ import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { RedisService } from 'src/redis/redis.service';
 import { md5 } from 'src/utils';
+import { Role } from './entities/role.entity';
+import { Permission } from './entities/permission.entity';
 @Injectable()
 export class UserService {
   private logger = new Logger();
   @InjectRepository(User)
   private userRepository: Repository<User>;
+
+  @InjectRepository(Role)
+  private roleRepository: Repository<Role>;
+
+  @InjectRepository(Permission)
+  private permissionRepository: Repository<Permission>;
 
   @Inject()
   private redisService: RedisService;
@@ -46,5 +54,49 @@ export class UserService {
       this.logger.error(error, UserService);
       throw new BadRequestException('注册失败');
     }
+  }
+
+  async initData() {
+    const user1 = new User();
+    user1.username = '张三';
+    user1.password = md5('123456');
+    user1.email = 'zhangsan@qq.com';
+    user1.nick_name = '管理员';
+
+    const user2 = new User();
+    user2.username = '李四';
+    user2.password = md5('123456');
+    user2.email = 'lisi@qq.com';
+    user2.nick_name = '普通用户';
+
+    const role1 = new Role();
+    role1.name = '管理员';
+    role1.description = '管理员';
+
+    const role2 = new Role();
+    role2.name = '普通用户';
+    role2.description = '普通用户';
+
+    const permission1 = new Permission();
+    permission1.code = 'admin';
+    permission1.description = '管理员';
+
+    const permission2 = new Permission();
+    permission2.code = 'user';
+    permission2.description = '普通用户';
+
+    user1.roles = [role1];
+    user2.roles = [role2];
+
+    role1.permissions = [permission1, permission2];
+    role2.permissions = [permission2];
+    
+    // 不能打乱顺序，因为存在外键依赖关系:
+    // 1. 角色依赖权限，所以必须先创建权限
+    // 2. 用户依赖角色，所以必须先创建角色
+    // 所以必须按照 permission -> role -> user 的顺序保存
+    await this.permissionRepository.save([permission1, permission2]);
+    await this.roleRepository.save([role1, role2]);
+    await this.userRepository.save([user1, user2]);
   }
 }
